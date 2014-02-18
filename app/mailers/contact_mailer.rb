2857c218a34f78e0fe73b23284f0a0051c941c6e
Federico Iachetti <iachetti.federico@gmail.com>
2014-02-18 19:20:24 -0300

    TEST 2

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,8 +4,10 @@ class ContactMailer < ActionMailer::Base
 
     apcvc_mail = "info.apcvc@gmail.com"
 
+    p ENV['SENDGRID_USERNAME']
+
     mail(to:       apcvc_mail,
-         from:     "#{ENV['SENDGRID_USERNAME']}@heroku.com",
+         from:     ENV['SENDGRID_USERNAME'],
          reply_to: message.email,
          subject: "[APCVC WEB] #{message.subject}")
   end
