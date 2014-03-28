aac39d53aa013fd4bd3731b7237dabf97a44149d
Federico Iachetti <iachetti.federico@gmail.com>
2014-03-27 23:16:33 -0300

    Fixed bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
     feed = Feed.new(Announcement.published.front_page, Event.published.front_page).to_a
     @announcements = feed
     @poster = get_poster
-    @photos = photos
   end
 
   private
@@ -11,11 +10,9 @@ class HomeController < ApplicationController
   def get_poster
     repo  = PicasaWebAlbums.get_repository(ENV["GOOGLE_USER"], ENV["GOOGLE_PASS"])
     album_id = repo.get_album_by_title("Portada").id
-    if album_id
-      photos = repo.get_photos_by_album_id(album_id)
-      photos.sort_by(&:id).last.url
-    else
-      "http://www.libreriapaidos.com/eventos/ImageFetch.ashx?Size=2&ImageID=5626"
-    end
+    photos = repo.get_photos_by_album_id(album_id)
+    photos.sort_by(&:id).last.url
+  rescue TypeError
+    "http://www.libreriapaidos.com/eventos/ImageFetch.ashx?Size=2&ImageID=5626"
   end
 end
